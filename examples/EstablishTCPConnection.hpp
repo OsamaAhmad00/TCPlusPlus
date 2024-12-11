0e1c88a464a684bda8bd58bf684619a7b9601b0e
Osama Ahmad <osama199915@gmail.com>
2024-12-11 09:58:20 +0300

    Initial implementation of the TCP state machine for establishing 3-way handshakes

--- examples/EstablishTCPConnection.hpp
+++ examples/EstablishTCPConnection.hpp
@@ -72,10 +72,24 @@ struct TCB {
     SendSequenceSpace send { };
     ReceiveSequenceSpace receive { };
 
+    enum class State {
+        New,
+        SynRcvd,  // Syn Received
+        SynSent,  // Syn Sent
+        Estab,    // Connection Established
+
+        WaitingForDataAck,  // TODO Delete this
+        WaitingForFinAck,   // TODO Delete this
+        Closed,             // TODO Delete this
+    } state = State::New ;
+
     void send_packet(tcpp::structs::IPv4& ip, const tcpp::TunDevice& tun) {
         auto& tcp = ip.tcp_payload();
-        tcp.set_seq_num(send.nxt + 1);
+        tcp.set_seq_num(send.nxt);
+        tcp.set_ack_num(receive.nxt);
         tcp.set_window_size(send.wnd);
+        std::swap(ip.source_addr_n, ip.dest_addr_n);
+        std::swap(tcp.source_port_n, tcp.dest_port_n);
         ip.compute_and_set_ip_tcp_checksums();
         auto buffer = reinterpret_cast<uint8_t*>(&ip);
         (void)tun.send(std::span { buffer, ip.total_len() });
@@ -85,7 +99,9 @@ struct TCB {
         send.nxt += seq_increase;
     }
 
-    void process_syn(const tcpp::structs::TCP& tcp) {
+    void process_syn(tcpp::structs::IPv4& ip, const tcpp::TunDevice& tun) {
+        auto& tcp = ip.tcp_payload();
+
         send.iss = 0;
         send.una = send.iss;
         send.nxt = send.iss;
@@ -94,55 +110,65 @@ struct TCB {
         receive.irs = tcp.seq_num();
         receive.nxt = receive.irs + 1;
         receive.wnd = tcp.window_size();
-    }
-
-    void process_packet(tcpp::structs::IPv4& ip, const tcpp::TunDevice& tun) {
-        auto& tcp = ip.tcp_payload();
-
-        if (tcp.syn != true) return;
-
-        process_syn(tcp);
 
         // Remove any options
         const auto old_offset = tcp.data_offset;
         tcp.data_offset = 5;
         ip.set_total_len(ip.total_len() - (old_offset - 5) * 4);
 
-        std::swap(ip.source_addr_n, ip.dest_addr_n);
-        std::swap(tcp.source_port_n, tcp.dest_port_n);
         tcp.ack = true;
-        tcp.set_ack_num(receive.nxt);
         send_packet(ip, tun);
 
-        // Wait for ack
-        // TODO Process an ack instead of just sleeping
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        state = State::SynRcvd;
+    }
 
-        // Send message
-        tcp.syn = false;
-        ip.set_tcp_payload("Hello World!\n");
-        send_packet(ip, tun);
-
-        // Wait for ack
-        // TODO Process an ack instead of just sleeping
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
-        // Send fin
-        ip.set_tcp_payload("");
-        tcp.ack = false;
-        tcp.fin = true;
-        send_packet(ip, tun);
-
-        // Wait for fin
-        // TODO Process a fin instead of just sleeping. This relies on
-        //  the client to finish the connection about before 3 seconds
-        std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-
+    void process_fin(tcpp::structs::IPv4& ip, const tcpp::TunDevice& tun) {
         // Send ack
-        tcp.set_ack_num(receive.nxt + 1);
+        auto& tcp = ip.tcp_payload();
         tcp.ack = true;
         tcp.fin = false;
         send_packet(ip, tun);
+    }
+
+    void process_ack(tcpp::structs::IPv4& ip, const tcpp::TunDevice& tun) {
+        auto& tcp = ip.tcp_payload();
+        if (state == State::SynRcvd) {
+            // Send message
+            ip.set_tcp_payload("Hello World!\n");
+            send_packet(ip, tun);
+            state = State::WaitingForDataAck;
+        } else if (state == State::WaitingForDataAck) {
+            // Send fin
+            ip.set_tcp_payload("");
+            tcp.ack = false;
+            tcp.fin = true;
+            send_packet(ip, tun);
+            state = State::WaitingForFinAck;
+        } else if (state == State::WaitingForFinAck) {
+            state = State::Closed;
+        }
+    }
+
+    void process_new(tcpp::structs::IPv4& ip, const tcpp::TunDevice& tun) {
+        auto& tcp = ip.tcp_payload();
+        // Ignore any non-syn packet for new connections
+        if (tcp.syn != true) return;
+        process_syn(ip, tun);
+    }
+
+    void process_packet(tcpp::structs::IPv4& ip, const tcpp::TunDevice& tun) {
+        if (state == State::New) {
+            return process_new(ip, tun);
+        }
+
+        auto& tcp = ip.tcp_payload();
+        receive.nxt += tcp.syn | tcp.fin;  // TODO syn or fin only?
+        if (tcp.ack == true) {
+            process_ack(ip, tun);
+        }
+        if (tcp.fin) {
+            process_fin(ip, tun);
+        }
     }
 };
 
